42d70a8079b5a3b1e23ffb8b0539e7f08c66ce74
mac <mac@macs-MacBook-Pro.local>
2021-06-30 09:59:46 -0500

    Updated the main.go files to work more as templates for other tools where the shellcode can easily be replaced

--- cmd/CreateRemoteThread/main.go
+++ cmd/CreateRemoteThread/main.go
@@ -33,7 +33,7 @@ func main() {
 	flag.Parse()
 
 	// Pop Calc Shellcode
-	shellcode, errShellcode := hex.DecodeString("505152535657556A605A6863616C6354594883EC2865488B32488B7618488B761048AD488B30488B7E3003573C8B5C17288B741F204801FE8B541F240FB72C178D5202AD813C0757696E4575EF8B741F1C4801FE8B34AE4801F799FFD74883C4305D5F5E5B5A5958C3")
+	shellcode, errShellcode := hex.DecodeString("%%SHELLCODE_HEX%%")
 	if errShellcode != nil {
 		log.Fatal(fmt.Sprintf("[!]there was an error decoding the string to a hex byte array: %s", errShellcode.Error()))
 	}
